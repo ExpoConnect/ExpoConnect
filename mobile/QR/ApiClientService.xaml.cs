e20148c6676e09cb5f0d892982b257071807a10c
goldi5863 <goldilishner@gmail.com>
2025-11-17 12:19:01 +0200

    Create Login page

---
+++ mobile/QR/ApiClientService.xaml.cs
@@ -0,0 +1,101 @@
+namespace QR;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//מחלקה אחת שמרכזת את כל הטיפול בבקשות לשרת, כולל:
+//שליחת בקשות ל־API(GET, POST וכו’)
+//הוספת Header עם ה־Access Token
+//טיפול אוטומטי במקרה של טוקן שפג תוקף
+//חידוש הטוקן ע"י ה־Refresh Token
+
+
+public partial class ApiClientService : ContentPage
+{
+	private readonly HttpClient _client;
+
+    public ApiClientService()
+	{
+        //הלקוח שאיתו מדברים עם  השרת
+        _client = new HttpClient
+        {
+            BaseAddress = new Uri("http://10.0.2.2:5027/api/")
+        };
+        InitializeComponent();
+
+	}
+    private async Task AddAuthorizationHeaderAsync()
+    {
+        var token = await SecureStorage.GetAsync("access_token");
+        if (!string.IsNullOrEmpty(token)) {
+            //מוסיף את ה־Access Token לכל בקשה שאת שולחת לשרת.
+            _client.DefaultRequestHeaders.Authorization =
+             new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+
+    public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+    {
+        await AddAuthorizationHeaderAsync();
+        var response = await _client.SendAsync(request);
+
+        //אם קיבלנו 401 ננסה לחדש את התוקן
+        if(response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            bool refreshed = await TryRefreshTokenAsync();
+            if (refreshed) {
+                await AddAuthorizationHeaderAsync();
+                response = await _client.SendAsync(request);
+            }
+        }
+        return response;
+    }
+
+    private async Task<bool> TryRefreshTokenAsync()
+    {
+        var refreshToken = await SecureStorage.GetAsync("refresh_token");
+        if (string.IsNullOrEmpty(refreshToken)) 
+            return false;
+
+        var json = JsonSerializer.Serialize(new { refreshToken });
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var refreshResponse = await _client.PostAsync("Auth/refresh",content);
+        if (!refreshResponse.IsSuccessStatusCode)
+            return false;
+        
+        var responseBody = await refreshResponse.Content.ReadAsStringAsync();
+        var tokens = JsonSerializer.Deserialize<AuthResponse>(responseBody);
+
+        if (tokens == null || string.IsNullOrEmpty(tokens.AccessToken))
+            return false;
+
+        await SecureStorage.SetAsync("access_token",tokens.AccessToken);
+        await SecureStorage.SetAsync("refresh_token", tokens.RefreshToken);
+
+        return true;
+    }
+
+    //שלוח בקשת POST לשרת 
+    public async Task<HttpResponseMessage> PostAsync(string endpoint ,object data)
+    {
+        //ממירה את האובייקט  לפורמט JSON.
+        var json = JsonSerializer.Serialize(data);
+        //יוצרים את גוף הבקשה (body)
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        //יוצרים אובייקט בקשה אמיתי לשרת
+        var request = new HttpRequestMessage(HttpMethod.Post, endpoint) {Content = content };
+        return await SendAsync(request);
+    }
+
+    public class AuthResponse
+    {
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+
+}
---
+++ mobile/QR/LoginPage.xaml.cs
@@ -0,0 +1,63 @@
+using System.Text.Json;
+
+namespace QR;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private async void OnRegisterClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new RegisterPage());
+
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+	{
+		string email = EmailEntry.Text;
+        string password = PasswordEntry.Text;
+        if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+        {
+            await DisplayAlert("Error", "Please fill all fields", "OK");
+            return;
+        }
+
+        try
+        {
+            var api = new ApiClientService();
+
+            var data = new { email = email, password = password };
+            var response = await api.PostAsync("Auth/login", data);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                //מפרקת (מפענחת) את הטקסט ששרת ה־API החזיר לתוך מבנה נתונים שניתן לגשת אליו בקוד
+                var jsonResponse = JsonSerializer.Deserialize<JsonElement>(responseBody);
+
+                string accessToken = jsonResponse.GetProperty("accessToken").GetString();
+                string refreshToken = jsonResponse.GetProperty("refreshToken").GetString();
+
+                // שמירה מאובטחת
+                await SecureStorage.SetAsync("access_token", accessToken);
+                await SecureStorage.SetAsync("refresh_token", refreshToken);
+
+                await DisplayAlert("Welcome", "Login successful!", "OK");
+            }
+            else
+            {
+                string error = await response.Content.ReadAsStringAsync();
+                await DisplayAlert("Error", $"Login failed: {error}", "OK");
+            }
+        }
+        catch (Exception ex) {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+
+
+    }
+}
